b3ff9e48412ca679a6e3186647fdd0be83e24211
seoho <ansndmin1@gmail.com>
2019-07-12 18:16:47 +0900

    Add styling for setting index screen

--- client/src/scenes/Settings/index.js
+++ client/src/scenes/Settings/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from "react-native";
 import AsyncStorage from "@react-native-community/async-storage";
+import LinearGradient from "react-native-linear-gradient";
+import Icon from "react-native-vector-icons/Entypo";
 
 import { getUserSetting } from "../../store/settings/actions";
 
@@ -17,39 +26,115 @@ class Settings extends Component {
   }
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: "#d2f3fc", paddingTop: 100 }}>
-        <View style={{ flexDirection: "row" }}>
-          <Text>설정</Text>
-          <Button
-            title="홈"
-            onPress={() => this.props.navigation.navigate("Main")}
-          />
+      <LinearGradient
+        colors={["#ffffff", "#d2f3fc"]}
+        style={styles.linearGradient}
+      >
+        <View style={styles.container}>
+          <View style={styles.headerViewStyle}>
+            <Text style={styles.headerTextStyle}>설정</Text>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate("Main")}
+            >
+              <Image
+                style={styles.closeImageStyle}
+                source={require("../../assets/images/icClose24.png")}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.settingBlockStyle}>
+            <Text style={styles.settingLabelStyle}>기본 정보</Text>
+            <View style={styles.settingBtnViewStyle}>
+              <TouchableOpacity
+                style={styles.settingBtnStyle}
+                onPress={() => this.props.navigation.navigate("MyInfo")}
+              >
+                <Text style={styles.settingTextStyle}>내 정보</Text>
+                <Icon name="chevron-right" color="#348dcd" size={16} />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.settingBlockStyle}>
+            <Text style={styles.settingLabelStyle}>알림</Text>
+            <View style={styles.settingBtnViewStyle}>
+              <TouchableOpacity
+                style={styles.settingBtnStyle}
+                onPress={() => this.props.navigation.navigate("Notification")}
+              >
+                <Text style={styles.settingTextStyle}>알림</Text>
+                <Icon name="chevron-right" color="#348dcd" size={16} />
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-        <View>
-          <Text>기본 정보</Text>
-          <Button
-            title="내 정보"
-            onPress={() => this.props.navigation.navigate("MyInfo")}
-          />
-        </View>
-        <View>
-          <Text>알림</Text>
-          <Button
-            title="알림"
-            onPress={() => this.props.navigation.navigate("Notification")}
-          />
-        </View>
-        {/* <View>
-          <Text>앱 정보</Text>
-          <Button
-            title="언어"
-            onPress={() => this.props.navigation.navigate("Language")}
-          />
-        </View> */}
-      </View>
+      </LinearGradient>
     );
   }
 }
+
+const screenWidth = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    alignItems: "center"
+  },
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  headerViewStyle: {
+    width: screenWidth,
+    height: 80,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: 84,
+    paddingRight: 30,
+    paddingTop: 57
+  },
+  headerTextStyle: {
+    fontFamily: "BMJUAOTF",
+    color: "#348dcd",
+    fontSize: 20
+  },
+  closeImageStyle: {
+    width: 24,
+    height: 24
+  },
+  settingBlockStyle: {
+    marginTop: 10,
+    paddingTop: 6,
+    paddingLeft: 84,
+    width: screenWidth
+  },
+  settingLabelStyle: {
+    fontFamily: "BMJUAOTF",
+    fontSize: 12,
+    color: "#999999",
+    lineHeight: 20
+  },
+  settingBtnViewStyle: {
+    width: screenWidth - 114,
+    height: 55,
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e7e7e7"
+  },
+  settingBtnStyle: {
+    width: screenWidth - 114,
+    height: 55,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row"
+  },
+  settingTextStyle: {
+    fontFamily: "BMJUAOTF",
+    fontSize: 16,
+    color: "#348dcd"
+  }
+});
 
 const mapDispatchToProps = dispatch => {
   return {
